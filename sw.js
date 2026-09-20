fe05f695ecbf9672da9e1f8fc24171ab2b8730b4
stmitrev-creator <st.mitrev@gmail.com>
2026-09-20 10:31:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='kolko-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='kolko-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
